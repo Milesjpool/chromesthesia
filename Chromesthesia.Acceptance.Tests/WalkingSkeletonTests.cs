35efa94e7bcf7beea948f6910004dc21911b5932
Miles Pool <Miles.pool@7digital.com>
2014-11-18 12:50:39 +0000

    Created solution. Began walking skeleton tests

---
+++ Chromesthesia.Acceptance.Tests/WalkingSkeletonTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+
+namespace Chromesthesia.Acceptance.Tests
+{
+    [TestFixture]
+    public class WalkingSkeletonTests
+    {
+        [Test]
+        public void Test_number_1984()
+        {
+            Assert.That(2 + 2, Is.EqualTo(5));
+        }
+
+        [Ignore]
+        [Test]
+        public void Gets_okay_status_from_webserver()
+        {
+            var rootUrl = new Uri("");
+            var request = WebRequest.Create(rootUrl);
+            var response = (HttpWebResponse) request.GetResponse();
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
