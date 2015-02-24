adfcba26535f0621b808b643e76542f1b4db9fec
Miles Pool <Miles.pool@7digital.com>
2015-02-24 18:19:00 +0000

    Made survey more user friendly

--- src/Chromesthesia.WebInterface/Content/Scripts/surveyScripts.js
+++ src/Chromesthesia.WebInterface/Content/Scripts/surveyScripts.js
@@ -1,47 +1,48 @@
 ﻿$(document).ready(function () {
-    
+
     var numOfTracks = tracks.length;
     var trackId = Math.floor(Math.random() * numOfTracks);
+    var paused = true;
 
-    $('#pause').click(function () {
-        $("#intro").show();
-        $("#intro").animate({ "top": "50%" }, 200);
-        pause();
-    });
-
-    $('#pause').hover(function () {
-        $(this).css("color", "#ffffff");
-    }, function () {
-        $(this).css( "color", "#000000");
-    });
-
-    $('#start').mousedown(function() {
+    $('#start').mousedown(function () {
         $(this).css("border-bottom", "3px solid #ccddff");
         $(this).css("border-top-width", "2px solid #5566dd");
         $(this).css("background-color", "#aabbee");
     });
-    
+
     $('#start').mouseup(function () {
         $(this).css("border-bottom", "1px solid #2233aa");
         $(this).css("border-top", "3px solid #ccddff");
         $(this).css("background-color", "#5566dd");
-        $("#intro").animate({ "top": "-50%" }, 200, function() {
-            $("#intro").hide();
-        });
-        play(trackId);
+        play();
     });
 
     $('#start').hover(function () {
-        $(this).animate({ "border-bottom-width":"2px", "border-top-width":"4px" }, 10);
+        $(this).animate({ "border-bottom-width": "2px", "border-top-width": "4px" }, 10);
     }, function () {
         $(this).animate({ "border-bottom-width": "1px", "border-top-width": "3px" }, 10);
     });
 
+    $('#playPause').click(function () {
+        if (paused) { play(); }
+        else { pause(); }
+    });
+
+    $('#skip').click(function () {
+        trackId = iterate(trackId, numOfTracks);
+        if (!paused) {
+            playTrack(trackId);
+        }
+    });
+
     $('.colour').click(function () {
+        $(this).animate({ "border-width": "5px" }, 100, function() {
+            $(this).animate({ "border-width": "2px" }, 200);
+        });
         var colour = $(this).css("background-color");
         saveResult(trackId, colour);
         trackId = iterate(trackId, numOfTracks);
-        play(trackId);
+        playTrack(trackId);
     });
 
     $('.colour').hover(function () {
@@ -50,19 +51,32 @@
         $(this).animate({ "border-width": "0" }, 100);
     });
 
-    function play(track) {
-        var $source = tracks[track];
+    function play() {
+        $("#playPause").attr("src", "content/Images/pause.png");
+        $("#introBackground").animate({ "top": "-50%" }, 200, function () {
+            $("#intro").hide();
+        });
+        playTrack(trackId);
+        paused = false;
+    }
+
+    function pause() {
+        $("#playPause").attr("src", "content/Images/play.png");
+        $("#intro").show();
+        $("#introBackground").animate({ "top": "50%" }, 200);
+        var $newSource = '<source id="audioSource" src="" type="audio/mp3"/>';
+        $("#audioSource").replaceWith($newSource);
+        $("audio").load();
+        paused = true;
+    }
+
+    function playTrack(id) {
+        var $source = tracks[id];
         var $newSource = '<source id="audioSource" src="Content/Clips/' + $source + '.mp3" type="audio/mp3"/>';
         $("#audioSource").replaceWith($newSource);
         $("audio").load();
     }
-    
-    function pause() {
-        var $newSource = '<source id="audioSource" src="" type="audio/mp3"/>';
-        $("#audioSource").replaceWith($newSource);
-        $("audio").load();
-    }
-    
+
     function iterate(id, maxVal) {
         id++;
         if (id >= maxVal) {
@@ -70,10 +84,9 @@
         }
         return id;
     }
-    
 
     function saveResult(id, colourString) {
-        
+
         Parse.initialize("LSqmQRHWpRXDusXRfFAPILmeMHBLabJ6KU7LeqJg", "ODK19A7OXkHtv68jqha46B2VwqSddRXxVLMcenlT");
         var Opinion = Parse.Object.extend("Opinion");
         var opinion = new Opinion();
@@ -81,9 +94,8 @@
         var rgb = colourString.substring(colourString.indexOf('(') + 1, colourString.lastIndexOf(')')).split(/,\s*/);
         opinion.save({ TrackId: id, TrackString: tracks[id], Colour: colourString, Redness: parseInt(rgb[0]), Greenness: parseInt(rgb[1]), Blueness: parseInt(rgb[2]) });
     }
-    
-});
 
+});
 
 var tracks = ["50 Cent - In Da Club",
     "Alexisonfire - Boiled Frogs",
